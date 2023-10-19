a5d08a6cf17e60b548223331b2c09619a091c7bc
Asajad Hussein <firstzpat@gmail.com>
2023-10-19 19:25:02 +0200

    added some error handling

--- Labs/labb_3.py
+++ Labs/labb_3.py
@@ -11,6 +11,8 @@ class Shape:
     
 class Rectangle(Shape):
     def __init__(self, x = 0, y = 0, side1 = 1, side2 = 1):
+        if side1 <= 0 or side2 <= 0:
+            raise ValueError("The side1 and side2 of the rectangle must be positive.")
         self.side1 = side1
         self.side2 = side2
         self.x = x
@@ -26,6 +28,8 @@ class Rectangle(Shape):
     
 class Circle(Shape):
     def __init__(self, x = 0, y = 0, radius = 1):
+        if radius <= 0:
+            raise ValueError("The radius of the circle must be positive.")
         self.radius = radius
         self.x = x
         self.y = y
