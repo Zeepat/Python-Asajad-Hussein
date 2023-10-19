76976a18075f0ac721da6b7c4c503d4442fe916d
Asajad Hussein <firstzpat@gmail.com>
2023-10-19 19:20:48 +0200

    added a rectangle class that inherits from Shape

--- Labs/labb_3.py
+++ Labs/labb_3.py
@@ -8,3 +8,10 @@ class Shape:
 
     def __str__(self):
         return f"A shape at ({self.x}, {self.y})"
+    
+class Rectangle(Shape):
+    def __init__(self, x = 0, y = 0, side1 = 1, side2 = 1):
+        self.side1 = side1
+        self.side2 = side2
+        self.x = x
+        self.y = y
